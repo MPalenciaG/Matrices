9d9127b9a9ec4e8d25cdc397d041711e2b3b9dbd
Mario Palencia <mpalenciag96@gmail.com>
2020-10-25 19:17:20 -0600

    actuallicé a .c

--- arreglos.c
+++ arreglos.c
@@ -6,16 +6,18 @@
 
 */
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(){
-  int matriz [2][3] = {{1,2,3},{4,5,6}};
+  int matriz [2][3] = {1,2,3,4,5,6};
   int x,y;
 
-  for (x=0;x<2;x++){
-    for (y=0;y<3;y++){
+  for (x=0;x<2;x++){ //bucle para cada fila
+    for (y=0;y<3;y++){ //bucle para cada columna
       printf("Digite un numero de Matriz [%i][%i]:\n", x+1,y+1);
-      scanf("%i ",&matriz[x][y]);
+      scanf("%i ", &matriz [x][y]);
     }
+    printf("\n");
   }
 
   for (x=0;x<2;x++){
@@ -27,4 +29,3 @@ int main(){
   getchar();
   return 0;
 }
-
